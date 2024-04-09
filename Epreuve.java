47435ebbd69af76dfe45d1942b84526d881b4214
Claire Deneau <claire.deneau@etu.univ-orleans.fr>
2024-04-09 14:51:33 +0200

    correction

--- Epreuve.java
+++ Epreuve.java
@@ -3,8 +3,8 @@ public interface Epreuve {
     public int participer(Equipe equipe);
     public int participer(Athlete athlete);
 
-    public bool aParticipe(Equipe equipe);
-    public bool aParticipe(Athlete athlete);
+    public boolean aParticipe(Equipe equipe);
+    public boolean aParticipe(Athlete athlete);
 
     public void ajouteScore(Equipe equipe, int score);
     public void ajouteScore(Athlete athlete, int score);
