2024604e093191d98b273ccd3388db5ccb77e8c0
Lucas Devers-Dore <lucas.devers-dore@etu.univ-orleans.fr>
2024-04-09 14:22:07 +0200

    début jeux olympiques

---
+++ JeuxOlympiques.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class JeuxOlympiques{
+    
+    private String dateDeDebut;
+    private String dateDeFin;
+    private List<Pays> listePays;
+
+    
+    
+}
