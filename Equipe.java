60afe28eb73050dc55f75a5aaa50ad4b8bda30f1
yohanstm <143810458+yohanstm@users.noreply.github.com>
2024-04-09 15:01:05 +0200

    Add files via upload
    ajout de la class Equipe

---
+++ Equipe.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class Equipe {
+    private int nbVictoire;
+    private List<Athlete> listeAthletes ;
+
+
+    public Equipe(){
+        this.listeAthletes = new ArrayList<>();
+    }
+
+    public Equipe(List<Athlete> listeAthletes , int nbVictoire){
+
+    }
+
+    public void ajouteAthlete(){
+
+    }
+
+    public void retirerAthlete(){
+
+    }
+}
