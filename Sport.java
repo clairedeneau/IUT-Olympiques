a60e6bb60ce2a13cf50e9966ecb2942dcf4e5e83
ArKoSs145 <dore.benjamin10@gmail.com>
2024-04-09 14:54:19 +0200

    Ajout attribut et methodes Sport

--- Sport.java
+++ Sport.java
@@ -1,3 +1,30 @@
-public class Sport{
+import java.util.HashMap;
+
+public abstract class Sport {
+
+    protected String sexe;
+    protected int distance;
+    protected String nom;
+    protected int tailleEquipe;
+    protected HashMap classement;
+
+    public String faireRegle() {
+
+    }
+
+    public Equipe match(Equipe autreEquipe) {
+
+    }
     
+    public Athlète match(Athlète autreEquipe) {
+
+    }
+
+    public int getNbVictoire() {
+
+    }
+
+    public double getClassement(Athlète participant) {
+        
+    }
 }
