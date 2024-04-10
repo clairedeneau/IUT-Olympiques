5a90f5c35237bb2e2f18fce838a1b965040beda8
Lucasdeversdore <147000878+Lucasdeversdore@users.noreply.github.com>
2024-04-10 11:31:32 +0200

    Petit probleme de branche
    Fichiers jo et pays

---
+++ JeuxOlympiques.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class JeuxOlympiques{
+    
+    private String dateDeDebut;
+    private String dateDeFin;
+    private List<Pays> listePays;
+
+    public JeuxOlympiques(String debut, String fin){
+        this.dateDeDebut = debut;
+        this.dateDeFin = fin;
+        this.listePays = new ArrayList<>();
+    }
+
+    public JeuxOlympiques(String debut, String fin, List<Pays> lesPays){
+        this.dateDeDebut = debut;
+        this.dateDeFin = fin;
+        this.listePays = lesPays;
+    }
+
+    public String getDateDeDebut() {
+        return dateDeDebut;
+    }
+
+    public void setDateDeDebut(String dateDeDebut) {
+        this.dateDeDebut = dateDeDebut;
+    }
+
+    public String getDateDeFin() {
+        return dateDeFin;
+    }
+
+    public void setDateDeFin(String dateDeFin) {
+        this.dateDeFin = dateDeFin;
+    }
+
+    public List<Pays> getListePays() {
+        return listePays;
+    }
+
+    public void setListePays(List<Pays> listePays) {
+        this.listePays = listePays;
+    }
+
+    public void ajoutePays(Pays unPays){
+        this.listePays.add(unPays);
+    }
+
+    public void ajoutePays(int nbOr, int nbArgent, int nbBronze, String nom, List<Athlete> lesAthletes){
+        this.listePays.add(new Pays(nbOr, nbArgent, nbBronze, nom, lesAthletes));
+    }
+   
+    public void ajoutePays(String nom){
+        this.listePays.add(new Pays(nom));
+    }
+
+    
+}   
---
+++ TestJeuxOlympiques.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestJeuxOlympiques {
+ 
+    @Test
+    public void testAjoutePays(){
+        List<Pays> test  =  new ArrayList<>();
+        Pays France = new Pays("France");
+        Pays USA = new Pays("USA");
+        Pays Belgique = new Pays("Belgique");
+        test.add(France);
+        test.add(USA);
+        test.add(Belgique);
+        JeuxOlympiques jo = new JeuxOlympiques("demain","dans 2 jours");
+        jo.ajoutePays(France);
+        jo.ajoutePays(USA);
+        jo.ajoutePays(Belgique);
+
+        assertEquals(test, jo.listePays);
+    }
+    
+}
