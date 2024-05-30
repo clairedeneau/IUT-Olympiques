49a8e57a409e192c2707fc54e23e0bcacdaf3b86
ArKoSs145 <dore.benjamin10@gmail.com>
2024-05-30 14:59:36 +0200

    Ajout Methodes sport

--- Sport.java
+++ Sport.java
@@ -1,4 +1,7 @@
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public abstract class Sport {
 
@@ -6,25 +9,75 @@ public abstract class Sport {
     protected int distance;
     protected String nom;
     protected int tailleEquipe;
-    protected HashMap classement;
+    protected HashMap<Integer, Athlete> classement;
+    protected int largeurTerrain;
+    protected int longueurTerrain;
+    protected int placeSpectateur;
 
-    public String faireRegle() {
-
+    public Sport(String sexe, int distance, String nom, int tailleEquipe, HashMap<Integer, Athlete> classement, int largeurTerrain, int longueurTerrain, int placeSpectateur){
+        this.sexe = sexe;
+        this.distance = distance;
+        this.nom = nom;
+        this.tailleEquipe = tailleEquipe;
+        this.classement = new HashMap<Integer, Athlete>();
+        this.largeurTerrain = largeurTerrain;
+        this.longueurTerrain = longueurTerrain;
+        this.placeSpectateur = placeSpectateur;
     }
 
-    public Equipe match(Equipe autreEquipe) {
-
-    }
-    
-    public Athlète match(Athlète autreEquipe) {
-
+    public String getSexe() {
+        return sexe;
     }
 
-    public int getNbVictoire() {
-
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
     }
 
-    public double getClassement(Athlète participant) {
-        
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getTailleEquipe() {
+        return tailleEquipe;
+    }
+
+    public void setTailleEquipe(int tailleEquipe) {
+        this.tailleEquipe = tailleEquipe;
+    }
+
+    public int getLargeurTerrain() {
+        return largeurTerrain;
+    }
+
+    public void setLargeurTerrain(int largeurTerrain) {
+        this.largeurTerrain = largeurTerrain;
+    }
+
+    public int getLongueurTerrain() {
+        return longueurTerrain;
+    }
+
+    public void setLongueurTerrain(int longueurTerrain) {
+        this.longueurTerrain = longueurTerrain;
+    }
+
+    public int getPlaceSpectateur() {
+        return placeSpectateur;
+    }
+
+    public void setPlaceSpectateur(int placeSpectateur) {
+        this.placeSpectateur = placeSpectateur;
     }
 }
