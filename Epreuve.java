37540ec41ad06c4ce3a31668811e13ef5a6d1065
Claire Deneau <claire.deneau@etu.univ-orleans.fr>
2024-06-07 14:55:36 +0200

    Ajout epreuve et score

--- Epreuve.java
+++ Epreuve.java
@@ -1,11 +1,46 @@
-public interface Epreuve {
+import java.util.List;
+import java.util.ArrayList;
+
+public class Epreuve {
+
+    private String nom;
+    private char sexe;
+    private List<Score<Integer, Double, Equipe>> classement;
+    private Sport sport;
+
+
+    public Epreuve(String nom, char sexe, Sport sport){
+        this.nom = nom;
+        this.sexe = sexe;
+        this.classement = new ArrayList<Score<Integer, Double, Equipe>>();
+        this.sport = sport;
+    }
     
-    public int participer(Equipe equipe);
-    public int participer(Athlete athlete);
+    public boolean contient(Equipe equipe){
+        for(Score<Integer, Double, Equipe> sc : this.classement){
+            if (sc.getParticipant().equals(equipe)){return true;}
+        }
+        return false;
+    }
 
-    public boolean aParticipe(Equipe equipe);
-    public boolean aParticipe(Athlete athlete);
+    public void participer(Equipe equipe, int classmt){
+        if (!(this.classement.contient(equipe))){
+            Score<Integer, Double, Equipe> scr = new Score<Integer, Double, Equipe>(classement, equipe);
+            this.classement.add(scr);
+        }
+    }
 
-    public void ajouteScore(Equipe equipe, int score);
-    public void ajouteScore(Athlete athlete, int score);
+    public void participer(Equipe equipe, int classmt, double pointsOuTemps){
+        if (!(this.classement.contient(equipe))){
+            Score<Integer, Double, Equipe> scr = new Score<Integer, Double, Equipe>(classement, pointsOuTemps, equipe);
+            this.classement.add(scr);
+        }
+    }
+    public void modifScore(Equipe equipe, int score){
+        for(Score<Integer, Double, Equipe> sc : this.classement){
+            if(sc.getParticipant().equals(Equipe)){
+                sc.setScore(score);
+            }
+        }
+    }
 }
