5d12bd8abd552856728a1b0d0d6a423129b2968f
clairedeneau <143797755+clairedeneau@users.noreply.github.com>
2024-06-13 09:14:00 +0200

    ajout docstrings

--- Epreuve.java
+++ Epreuve.java
@@ -10,6 +10,13 @@ public class Epreuve {
 
 
     public Epreuve(String nom, char sexe, Sport sport){
+        /**
+     * Constructeur pour initialiser une épreuve avec son nom, le sexe des participants et le sport associé.
+     *
+     * @param nom Le nom de l'épreuve.
+     * @param sexe Le sexe des participants ('M' pour masculin, 'F' pour féminin).
+     * @param sport Le sport associé à l'épreuve.
+     */
         this.nom = nom;
         this.sexe = sexe;
         this.classement = new ArrayList<Score<Integer, Double, Equipe>>();
@@ -17,6 +24,12 @@ public class Epreuve {
     }
     
     public boolean contient(Equipe equipe){
+        /**
+     * Vérifie si une équipe participe à l'épreuve.
+     *
+     * @param equipe L'équipe à vérifier.
+     * @return true si l'équipe participe, sinon false.
+     */
         for(Score<Integer, Double, Equipe> sc : this.classement){
             if (sc.getParticipant().equals(equipe)){return true;}
         }
@@ -24,6 +37,12 @@ public class Epreuve {
     }
 
     public void participer(Equipe equipe, int classmt){
+        /**
+     * Ajoute une équipe au classement de l'épreuve avec son classement.
+     *
+     * @param equipe L'équipe à ajouter.
+     * @param classmt Le classement de l'équipe.
+     */
         if (!(this.classement.contient(equipe))){
             Score<Integer, Double, Equipe> scr = new Score<Integer, Double, Equipe>(classement, equipe);
             this.classement.add(scr);
@@ -31,12 +50,25 @@ public class Epreuve {
     }
 
     public void participer(Equipe equipe, int classmt, double pointsOuTemps){
+        /**
+     * Ajoute une équipe au classement de l'épreuve avec son classement et ses points ou son temps.
+     *
+     * @param equipe L'équipe à ajouter.
+     * @param classmt Le classement de l'équipe.
+     * @param pointsOuTemps Les points ou le temps de l'équipe.
+     */
         if (!(this.classement.contient(equipe))){
             Score<Integer, Double, Equipe> scr = new Score<Integer, Double, Equipe>(classement, pointsOuTemps, equipe);
             this.classement.add(scr);
         }
     }
     public void modifScore(Equipe equipe, int score){
+        /**
+     * Modifie le score d'une équipe participante.
+     *
+     * @param equipe L'équipe dont le score doit être modifié.
+     * @param score Le nouveau score de l'équipe.
+     */
         for(Score<Integer, Double, Equipe> sc : this.classement){
             if(sc.getParticipant().equals(Equipe)){
                 sc.setScore(score);
