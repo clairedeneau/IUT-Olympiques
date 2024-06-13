f8ac26cf85cb78fb5afe8979094d02f4ae4389de
clairedeneau <143797755+clairedeneau@users.noreply.github.com>
2024-06-13 09:19:36 +0200

    ajout docstring

--- Score.java
+++ Score.java
@@ -5,38 +5,81 @@ public class Score<Integer, Double, Equipe> {
     private Equipe participant;
 
     public Score(Integer classement, Double score, Equipe participant){
+        /**
+     * Constructeur pour initialiser un score avec une position, un score et un participant.
+     *
+     * @param position La position (classement) du participant.
+     * @param score Le score du participant.
+     * @param participant Le participant (équipe).
+     */
         this.position = classement;
         this.score = score;
         this.participant = participant;
     }
 
     public Score(Integer classement, Equipe participant){
+        /**
+     * Constructeur pour initialiser un score avec une position et un participant, sans score.
+     *
+     * @param position La position (classement) du participant.
+     * @param participant Le participant (équipe).
+     */
         this.position = classement;
         this.score = null;
         this.participant = participant;
     }
 
     public Integer getPosition() {
+        /**
+     * Obtient la position du participant.
+     *
+     * @return La position du participant.
+     */
         return position;
     }
 
     public void setPosition(Integer position) {
+        /**
+     * Modifie la position du participant.
+     *
+     * @param position La nouvelle position du participant.
+     */
         this.position = position;
     }
 
     public Double getScore() {
+        /**
+     * Obtient le score du participant.
+     *
+     * @return Le score du participant.
+     */
         return score;
     }
 
     public void setScore(Double score) {
+        /**
+     * Modifie le score du participant.
+     *
+     * @param score Le nouveau score du participant.
+     */
         this.score = score;
     }
 
     public Equipe getParticipant() {
+        /**
+     * Obtient le participant.
+     *
+     * @return Le participant.
+     */
         return participant;
     }
 
     public void setParticipant(Equipe participant) {
+        /**
+     * Modifie le participant.
+     *
+     * @param participant Le nouveau participant.
+     */
         this.participant = participant;
     }
 
@@ -57,6 +100,12 @@ public class Score<Integer, Double, Equipe> {
 
     @Override
     public boolean equals(Object obj) {
+        /**
+     * Vérifie si cet objet Score est égal à un autre objet.
+     *
+     * @param obj L'objet à comparer avec cet objet Score.
+     * @return true si les objets sont égaux, sinon false.
+     */
         if (this == obj)
             return true;
         if (obj == null)
