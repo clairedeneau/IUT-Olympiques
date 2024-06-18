2d097578cf12c6492a4ba903f6ddf81776a238de
ArKoSs145 <dore.benjamin10@gmail.com>
2024-06-18 10:23:48 +0200

    Refonte des classes Sports

--- Sport.java
+++ Sport.java
@@ -4,66 +4,18 @@
  */
 public abstract class Sport {
 
-    protected int distance;
-    protected String nom;
     protected int tailleEquipe;
-    protected int largeurTerrain;
-    protected int longueurTerrain;
     protected int placeSpectateur;
 
     /**
      * Constructeur de la classe Sport.
      *
-     * @param distance        la distance associée au sport (par exemple, longueur de la course)
-     * @param nom             le nom du sport
      * @param tailleEquipe    le nombre de joueurs par équipe
-     * @param largeurTerrain  la largeur du terrain
-     * @param longueurTerrain la longueur du terrain
      * @param placeSpectateur le nombre de places disponibles pour les spectateurs
      */
-    public Sport(int distance, String nom, int tailleEquipe, int largeurTerrain, int longueurTerrain, int placeSpectateur) {
-        this.distance = distance;
-        this.nom = nom;
+    public Sport(int tailleEquipe, int placeSpectateur) {
         this.tailleEquipe = tailleEquipe;
-        this.largeurTerrain = largeurTerrain;
-        this.longueurTerrain = longueurTerrain;
         this.placeSpectateur = placeSpectateur;
-    }
-
-    /**
-     * Obtient la distance associée au sport.
-     *
-     * @return la distance du sport
-     */
-    public int getDistance() {
-        return distance;
-    }
-
-    /**
-     * Définit la distance associée au sport.
-     *
-     * @param distance la nouvelle distance du sport
-     */
-    public void setDistance(int distance) {
-        this.distance = distance;
-    }
-
-    /**
-     * Obtient le nom du sport.
-     *
-     * @return le nom du sport
-     */
-    public String getNom() {
-        return nom;
-    }
-
-    /**
-     * Définit le nom du sport.
-     *
-     * @param nom le nouveau nom du sport
-     */
-    public void setNom(String nom) {
-        this.nom = nom;
     }
 
     /**
@@ -82,42 +34,6 @@ public abstract class Sport {
      */
     public void setTailleEquipe(int tailleEquipe) {
         this.tailleEquipe = tailleEquipe;
-    }
-
-    /**
-     * Obtient la largeur du terrain.
-     *
-     * @return la largeur du terrain
-     */
-    public int getLargeurTerrain() {
-        return largeurTerrain;
-    }
-
-    /**
-     * Définit la largeur du terrain.
-     *
-     * @param largeurTerrain la nouvelle largeur du terrain
-     */
-    public void setLargeurTerrain(int largeurTerrain) {
-        this.largeurTerrain = largeurTerrain;
-    }
-
-    /**
-     * Obtient la longueur du terrain.
-     *
-     * @return la longueur du terrain
-     */
-    public int getLongueurTerrain() {
-        return longueurTerrain;
-    }
-
-    /**
-     * Définit la longueur du terrain.
-     *
-     * @param longueurTerrain la nouvelle longueur du terrain
-     */
-    public void setLongueurTerrain(int longueurTerrain) {
-        this.longueurTerrain = longueurTerrain;
     }
 
     /**
--- VolleyBall.java
+++ VolleyBall.java
@@ -1,28 +1,120 @@
 public class VolleyBall extends Sport{
 
+    // Attributs privés
     private boolean sableTerrain;
     private boolean salleTerrain;
+    private int largeurTerrain;
+    private int longueurTerrain;
+    private String nom;
 
-    public VolleyBall(int distance, String nom, int tailleEquipe, int largeurTerrain, int longueurTerrain, int placeSpectateur, boolean sableTerrain, boolean salleTerrain){
-        super(distance, nom, tailleEquipe, largeurTerrain, longueurTerrain, placeSpectateur);
+    /**
+     * Constructeur de la classe VolleyBall.
+     * 
+     * @param nom le nom de la discipline (par défaut "Volley-Ball")
+     * @param tailleEquipe la taille de l'équipe participant à la discipline
+     * @param largeurTerrain la largeur du terrain de volley-ball
+     * @param longueurTerrain la longueur du terrain de volley-ball
+     * @param placeSpectateur le nombre de places pour les spectateurs
+     * @param sableTerrain indique si le terrain est en sable
+     * @param salleTerrain indique si le terrain est en salle
+     */
+    public VolleyBall(String nom, int tailleEquipe, int largeurTerrain, int longueurTerrain, int placeSpectateur, boolean sableTerrain, boolean salleTerrain){
+        super(tailleEquipe, placeSpectateur);
         this.sableTerrain = sableTerrain;
         this.salleTerrain = salleTerrain;
+        this.largeurTerrain = largeurTerrain;
+        this.longueurTerrain = longueurTerrain;
+        this.nom = "Volley-Ball";
     }
 
+    /**
+     * Vérifie si le terrain est en sable.
+     * 
+     * @return true si le terrain est en sable, sinon false
+     */
     public boolean isSableTerrain() {
         return sableTerrain;
     }
 
+    /**
+     * Définit si le terrain est en sable.
+     * 
+     * @param sableTerrain true pour indiquer que le terrain est en sable, sinon false
+     */
     public void setSableTerrain(boolean sableTerrain) {
         this.sableTerrain = sableTerrain;
     }
 
+    /**
+     * Vérifie si le terrain est en salle.
+     * 
+     * @return true si le terrain est en salle, sinon false
+     */
     public boolean isSalleTerrain() {
         return salleTerrain;
     }
 
+    /**
+     * Définit si le terrain est en salle.
+     * 
+     * @param salleTerrain true pour indiquer que le terrain est en salle, sinon false
+     */
     public void setSalleTerrain(boolean salleTerrain) {
         this.salleTerrain = salleTerrain;
+    }
+
+    /**
+     * Obtient la largeur du terrain de volley-ball.
+     * 
+     * @return la largeur du terrain
+     */
+    public int getLargeurTerrain() {
+        return largeurTerrain;
+    }
+
+    /**
+     * Définit la largeur du terrain de volley-ball.
+     * 
+     * @param largeurTerrain la nouvelle largeur du terrain
+     */
+    public void setLargeurTerrain(int largeurTerrain) {
+        this.largeurTerrain = largeurTerrain;
+    }
+
+    /**
+     * Obtient la longueur du terrain de volley-ball.
+     * 
+     * @return la longueur du terrain
+     */
+    public int getLongueurTerrain() {
+        return longueurTerrain;
+    }
+
+    /**
+     * Définit la longueur du terrain de volley-ball.
+     * 
+     * @param longueurTerrain la nouvelle longueur du terrain
+     */
+    public void setLongueurTerrain(int longueurTerrain) {
+        this.longueurTerrain = longueurTerrain;
+    }
+
+    /**
+     * Obtient le nom de la discipline.
+     * 
+     * @return le nom de la discipline
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * Définit le nom de la discipline.
+     * 
+     * @param nom le nouveau nom de la discipline
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
     }
     
 }
