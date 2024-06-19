eac549a76e6a09eb4eb6e807450151d77dae553a
Lucas Devers-Dore <lucas.devers-dore@etu.univ-orleans.fr>
2024-06-19 14:39:30 +0200

    createur controleur et navigation entre les pages

---
+++ ControleurAdminAthlete.java
@@ -0,0 +1,59 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+public class ControleurAdminAthlete {
+
+        //Fx id de page_admin_modifAtglete
+    @FXML
+    private Label userNameA;
+
+    @FXML
+    private Button boutonLogOut;
+
+    @FXML
+    private Button boutnNewAcc;
+
+    @FXML
+    private TextField tfNom;
+
+    @FXML
+    private TextField tfPrenom;
+
+    @FXML
+    private RadioButton rbFemme;
+
+    @FXML
+    private RadioButton rbHomme;
+
+    @FXML
+    private Spinner spinnerForce;
+
+    @FXML
+    private Spinner spinnerAgilite;
+
+    @FXML
+    private Spinner spinnerEndur;
+
+    @FXML
+    private Spinner spinnerNbVict;
+
+    @FXML
+    private TextField tfPays;
+
+    @FXML
+    private Button boutonRetour;
+
+    @FXML
+    private Button boutonAnnuler;
+
+    @FXML
+    private Button boutonSup;
+
+    @FXML
+    private Button boutonSave;
+    
+}
--- IUTO.java
+++ IUTO.java
@@ -20,27 +20,18 @@ import javafx.stage.Stage;
 public class IUTO extends Application {
 
     private BorderPane panelCentral;
-
-    private BorderPane pageOrganisateur;
     
-    private BorderPane pageAccueil;
-
-    private BorderPane pageConnexion;
-
-    private BorderPane pageAdmin;
-
-    private BorderPane pageJournaliste;
-
-    private BorderPane pageAdminAthlete;
-    
-    private BorderPane pageAdminPays;
-
-    private BorderPane pageAdminCompte;
-
-
     private ConnexionMySQL connexionMySQL;
+    
+    private ControleurAccueil controleurAccueil;
+    
+    private ControleurConnexion controleurConnexion;
 
- 
+    private ControleurJournaliste controleurJournaliste;
+
+    private ControleurOrganisateurAccueil controleurOrganisateurAccueil;
+
+    private ControleurOrganisteurEpreuve controleurOrganisteurEpreuve;
 
     @Override
     public void init() throws IOException, ClassNotFoundException {
@@ -53,63 +44,121 @@ public class IUTO extends Application {
             System.exit(1);
         }
         this.panelCentral = new BorderPane();
-        this.pageAccueil = FXMLLoader.load(getClass().getResource("page_accueil.fxml"));
-        this.pageConnexion = FXMLLoader.load(getClass().getResource("page_connexion.fxml"));
-        this.pageOrganisateur = FXMLLoader.load(getClass().getResource("page_organisateur.fxml"));
-        this.pageJournaliste = FXMLLoader.load(getClass().getResource("page_journaliste.fxml"));
-        this.pageAdmin = FXMLLoader.load(getClass().getResource("page_admin_principale.fxml"));
-        this.pageAdminAthlete = FXMLLoader.load(getClass().getResource("page_admin_modifAthlete.fxml"));
-        this.pageAdminCompte = FXMLLoader.load(getClass().getResource("page_admin_nvCompte.fxml"));
-        this.pageAdminPays = FXMLLoader.load(getClass().getResource("page_admin_modifPays.fxml"));
-    } 
-
-
-
-    public void modeConnexion() {
-        this.panelCentral = this.pageConnexion;
+        controleurAccueil = new ControleurAccueil(this);
+        controleurConnexion = new ControleurConnexion(this);
+        controleurJournaliste = new ControleurJournaliste(this);
+        controleurOrganisateurAccueil = new ControleurOrganisateurAccueil(this);
+        controleurOrganisteurEpreuve = new ControleurOrganisteurEpreuve(this);
+    }
+        
+    public BorderPane loadPageAccueil() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("page_accueil.fxml"));
+        loader.setControllerFactory(c -> new ControleurAccueil(this));
+        loader.setController(controleurAccueil);
+        BorderPane root = loader.load();
+        return root;
+    }
+    
+    public BorderPane loadPageConnexion() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("page_connexion.fxml"));
+        loader.setControllerFactory(c -> new ControleurConnexion(this));
+        loader.setController(controleurConnexion);
+        BorderPane root = loader.load();
+        return root;
+    }
+    
+    public BorderPane loadPageOrganisateurAccueil() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("page_orgaAccueil.fxml"));
+        loader.setControllerFactory(c -> new ControleurOrganisateurAccueil(this));
+        loader.setController(controleurOrganisateurAccueil);
+        BorderPane root = loader.load();
+        return root;
     }
 
-    public void modeAccueil() {
-        this.panelCentral = this.pageAccueil;
+    public BorderPane loadPageOrganisateurEpreuve() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("page_orgaEpreuve.fxml"));
+        loader.setControllerFactory(c -> new ControleurOrganisateurAccueil(this));
+        loader.setController(controleurOrganisteurEpreuve);
+        BorderPane root = loader.load();
+        return root;
+    }
+    
+    public BorderPane loadPageJournaliste() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("page_journaliste.fxml"));
+        loader.setControllerFactory(c -> new ControleurJournaliste(this));
+        loader.setController(controleurJournaliste);
+        BorderPane root = loader.load();
+        return root;
+    }
+    
+    public BorderPane loadPageAdmin() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("page_admin_principale.fxml"));
+        return loader.load();
+    }
+    
+    public BorderPane loadPageAdminAthlete() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("page_admin_modifAthlete.fxml"));
+        return loader.load();
+    }
+    
+    public BorderPane loadPageAdminCompte() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("page_admin_nvCompte.fxml"));
+        return loader.load();
+    }
+    
+    public BorderPane loadPageAdminPays() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("page_admin_modifPays.fxml"));
+        return loader.load();
+    }
+    
+    public void modeConnexion() throws IOException {
+        this.panelCentral.setCenter(loadPageConnexion());
+    }
+    
+    public void modeAccueil() throws IOException {
+        this.panelCentral.setCenter(loadPageAccueil());
+    }
+    
+    public void modeOrganisateurAccueil() throws IOException {
+        this.panelCentral.setCenter(loadPageOrganisateurAccueil());
     }
 
-    public void modeOrganisateur() {
-        this.panelCentral = this.pageOrganisateur;
+    public void modeOrganisateurEpreuve() throws IOException {
+        this.panelCentral.setCenter(loadPageOrganisateurEpreuve());
     }
-
-    public void modeJournaliste() {
-        this.panelCentral = this.pageJournaliste;
+    
+    public void modeJournaliste() throws IOException {
+        this.panelCentral.setCenter(loadPageJournaliste());
     }
-
-    public void modeAdmin() {
-        this.panelCentral = this.pageAdmin;
+    
+    public void modeAdmin() throws IOException {
+        this.panelCentral.setCenter(loadPageAdmin());
     }
-
-    public void modeAdminPays() {
-        this.panelCentral = this.pageAdminPays;
+    
+    public void modeAdminPays() throws IOException {
+        this.panelCentral.setCenter(loadPageAdminPays());
     }
-
-    public void modeAdminCompte() {
-        this.panelCentral = this.pageAdminCompte;
+    
+    public void modeAdminCompte() throws IOException {
+        this.panelCentral.setCenter(loadPageAdminCompte());
     }
-
-    public void modeAdminAthlete() {
-        this.panelCentral = this.pageAdminAthlete;
+    
+    public void modeAdminAthlete() throws IOException {
+        this.panelCentral.setCenter(loadPageAdminAthlete());
     }
-
-
+    
+    
     @Override
     public void start(Stage stage) throws IOException, SQLException {
 
-        connexionMySQL.connecter("servinfo-maria", "DBdore", "dore", "dore");
-        this.panelCentral = this.pageAccueil;
+        modeAccueil();
         stage.setTitle("Jeux Olympiques 2024");
-        Scene scene = new Scene(this.panelCentral);
+        Scene scene = new Scene(this.panelCentral, 600, 600);
         stage.setScene(scene);
         stage.show();
-
+    
     }
-
+    
     public static void main(String[] args) {
         launch(args);
     }
